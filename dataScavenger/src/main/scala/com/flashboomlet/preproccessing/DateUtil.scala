95b5cc0e8061d3276edd40311de16015f3a6b658
ttlynch <lyncht@unm.edu>
2016-06-18 09:07:05 -0600

    Addition of DateUtil. \n\n - Takes dates and converts them to ISO format. \n - Takes dates and normalizes them.

---
+++ dataScavenger/src/main/scala/com/flashboomlet/preproccessing/DateUtil.scala
@@ -0,0 +1,64 @@
+package com.flashboomlet.preproccessing
+
+import java.time.Instant
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import java.util.Date
+
+/**
+  * DateUtil is used to handle the date conversions from various sources into the proper
+  * mongoDB Date/Time format to conform to the BSON ISODate format
+  *
+  * Converts all dates to ISO format in a normalized standard using the UTC time standard.
+  *
+  * http://stackoverflow.com/questions/3778428/best-way-to-store-date-time-in-mongodb
+  *
+  */
+object DateUtil {
+
+  /**
+    * Formatter for twitter dates.
+    *
+    * @return a formatter for Twitter Date Times
+    */
+  private def twitterFormatter(): DateTimeFormatter = {
+    DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss ZZZZZ yyyy").withZone(ZoneOffset.UTC)
+  }
+
+  /**
+    * Formatter for ISO Dates
+    *
+    * @return a formatter for ISO Date Times
+    */
+  private def isoFormatter(): DateTimeFormatter = {
+    DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mmZ").withZone(ZoneOffset.UTC)
+  }
+
+  /**
+    * getToday returns today's date time in iso format
+    * @return iso formatted dateTime
+    */
+  def getToday(): String = {
+    isoFormatter.format(Instant.now())
+  }
+
+  /**
+    * convertTwitterDate converts a twitters date to the proper ISO format on the UTC time standard
+    *
+    * @param date twitter date
+    * @return iso formatted dateTime in a String
+    */
+  def convertTwitterDate(date: Date): String = {
+    isoFormatter.format(twitterFormatter.parse(date.toString))
+  }
+
+  /**
+    * normalizeDate takes a string and normalizes the date to UTC time
+    *
+    * @param date a date to be normalized
+    * @return a normalized date
+    */
+  def normalizeDate(date: String): String = {
+    isoFormatter.format(isoFormatter.parse(date))
+  }
+}
